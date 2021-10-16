091c7bacfd3f9f74fc64e2f9493f8ebfcfe7d1cb
alfin.pratama <alfin.pratama@dana.id>
2021-10-16 14:07:57 +0700

    upgrade v2

--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func SayHello(name string) string {
-	return "Halo selamat datang " + name
+func SayHello(name string, mrOrs string) string {
+	return "Halo selamat datang "+ mrOrs+ name
 }
